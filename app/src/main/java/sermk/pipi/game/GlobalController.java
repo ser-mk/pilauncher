d11923e35b58014a93b23af42f3161d61140c346
Evgeny Chormonov <en.chormonov@gmail.com>
2017-10-30 22:07:23 +0300

    FEAT: add global controller

---
+++ app/src/main/java/sermk/pipi/game/GlobalController.java
@@ -0,0 +1,34 @@
+package sermk.pipi.game;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+/**
+ * Created by echormonov on 30.10.17.
+ */
+
+public final class GlobalController extends Application {
+
+    // Called when the application is starting, before any other application objects have been created.
+    // Overriding this method is totally optional!
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Required initialization logic here!
+    }
+
+    // Called by the system when the device configuration changes while your component is running.
+    // Overriding this method is totally optional!
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    // This is called when the overall system is running low on memory,
+    // and would like actively running processes to tighten their belts.
+    // Overriding this method is totally optional!
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+}
