42247d3b77bbd7364af75ad1eda94a32ea451f1a
Evgeny Chormonov <en.chormonov@gmail.com>
2017-10-30 22:06:51 +0300

    FEAT: ad tooglebutton to test fragment

--- app/src/main/java/sermk/pipi/game/TestCV_Fragment.java
+++ app/src/main/java/sermk/pipi/game/TestCV_Fragment.java
@@ -3,27 +3,41 @@ package sermk.pipi.game;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  */
-public class TestCV_Fragment extends Fragment {
+class TestCV_Fragment extends Fragment {
+
+    private final String TAG  = "TestCV_Fragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    private ToggleButton mPreviewButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_test_cv_, container, false);
+
+        mPreviewButton = (ToggleButton)rootView.findViewById(R.id.start_test);
+        setPreviewButton(false);
+        mPreviewButton.setOnCheckedChangeListener(mOnCheckedChangeListener);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test_cv_, container, false);
+        return rootView;
     }
 
     @Override
@@ -35,6 +49,32 @@ public class TestCV_Fragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private final CompoundButton.OnCheckedChangeListener mOnCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(final CompoundButton buttonView, final boolean isChecked) {
+            Log.v(TAG, "Test CV enable " + isChecked);
+            if (isChecked) {
+
+            } else {
+
+            }
+        }
+    };
+
+    private void setPreviewButton(final boolean onoff) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mPreviewButton.setOnCheckedChangeListener(null);
+                try {
+                    mPreviewButton.setChecked(onoff);
+                } catch (Exception e) {
+                    Log.w(TAG, e);
+                }
+            }
+        });
     }
 
 }
