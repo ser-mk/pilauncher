b8f182370cef79effb9b2abe3cc3c3c6ecb82b6b
Evgeny Chormonov <en.chormonov@gmail.com>
2017-10-30 19:47:29 +0300

    add Test CV fragment skeleton

--- app/src/main/java/sermk/pipi/game/Standalone.java
+++ app/src/main/java/sermk/pipi/game/Standalone.java
@@ -2,10 +2,6 @@ package sermk.pipi.game;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Standalone extends Activity {
 
@@ -15,6 +11,9 @@ public class Standalone extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standalone);
+
+        getFragmentManager().beginTransaction()
+                .add(R.id.container, new TestCV_Fragment()).commit();
 
     }
 
---
+++ app/src/main/java/sermk/pipi/game/TestCV_Fragment.java
@@ -0,0 +1,40 @@
+package sermk.pipi.game;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ */
+public class TestCV_Fragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_test_cv_, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
