8eab5577926ac1f75f8ac6f34b6f9928aa1fed66
Evgeny Chormonov <en.chormonov@gmail.com>
2018-01-08 19:20:55 +0300

    FEAT: add welcome fragment

---
+++ app/src/main/java/sermk/pipi/pilauncher/GUIFragment/WelcomeFragment.java
@@ -0,0 +1,42 @@
+package sermk.pipi.pilauncher.GUIFragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import sermk.pipi.pilauncher.R;
+
+
+public class WelcomeFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView =  inflater.inflate(R.layout.fragment_welcome, container, false);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
