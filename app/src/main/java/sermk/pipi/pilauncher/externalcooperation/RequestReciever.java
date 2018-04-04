d2b00c4dcac969bac03bfe02ca319c36883db936
Evgeny Chormonov <en.chormonov@gmail.com>
2018-04-04 15:05:27 +0300

    FEAT: add request settings reciver

---
+++ app/src/main/java/sermk/pipi/pilauncher/externalcooperation/RequestReciever.java
@@ -0,0 +1,53 @@
+package sermk.pipi.pilauncher.externalcooperation;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import sermk.pipi.pilib.CommandCollection;
+import sermk.pipi.pilib.ErrorCollector;
+import sermk.pipi.pilib.MClient;
+import sermk.pipi.pilib.UniversalReciver;
+
+public class RequestReciever extends BroadcastReceiver {
+
+    private String TAG = this.getClass().getName();
+
+    private final ErrorCollector EC = new ErrorCollector();
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        EC.clear();
+
+        final UniversalReciver.ReciverVarible rv
+            = UniversalReciver.parseIntent(intent, TAG);
+
+        String error = ErrorCollector.NO_ERROR;
+
+        try {
+            error = doAction(context, rv.action);
+        } catch (Exception e){
+            e.printStackTrace();
+            error = e.toString();
+        }
+
+        if(ErrorCollector.NO_ERROR.equals(error)) return;
+
+        EC.addError(error);
+
+        Log.v(TAG, EC.error);
+
+        MClient.sendMessage(context, EC.subjError(TAG, rv.action), EC.error);
+    }
+
+    private String doAction(Context context, final String action){
+        if (!CommandCollection.ACTION_RECIVER_FOR_ALL_QUERY_SETTINGS.equals(action)){
+            return "Undefined action!";
+        }
+
+        AllSettings.getInstance().confirmSettings(context);
+
+        return ErrorCollector.NO_ERROR;
+    }
+}
